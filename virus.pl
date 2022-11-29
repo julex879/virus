bd938cd99b4fab1ef70ccdad45bebb5a936955b4
julio_llas@outlook.com <julio_llas@outlook.com>
2022-11-29 15:12:46 -0600

    animacion

--- virus.pl
+++ virus.pl
@@ -1,8 +1,19 @@
 #!/usr/bin/perl
-
 # Modules used
 use strict;
 use warnings;
-
+use Time::HiRes qw(usleep);
+local $| = 1;
 # Print function
 print("Hello World\n");
+print("YOU ARE BEING INFECTED\n");
+system("perl --version ");
+print("Delete files:");
+my @nums = 1 .. 100;
+foreach my $c (@nums) {
+print "$c%";
+usleep(50000);
+print ("\b" x (length($c)+1));
+
+}
+print("System 32 deleted, have a good day :)");
